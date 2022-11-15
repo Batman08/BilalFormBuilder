4bc7ccf545325f0cac2b7b31e618986f8f6cbff4
bilal asghar <asgharbilal8@gmail.com>
2022-11-15 12:26:33 +0000

    add form element through drag event

--- MySaaSProject/wwwroot/typescript/FormBuilder.js
+++ MySaaSProject/wwwroot/typescript/FormBuilder.js
@@ -1,30 +1,76 @@
 /// <reference types="./FormBuilder" />
 /// <reference path="./FormElements.ts" />
 class FormBuilder {
+    /*private readonly _formElements = document.querySelectorAll(".formElementWrapper") as NodeListOf<HTMLDivElement>;*/
     constructor() {
         this._customFormSection = document.querySelector("#customFormSection");
-        this._formElements = document.querySelectorAll(".formElementWrapper");
         this.AddFormElement();
     }
     AddFormElement() {
-        this._formElements.forEach((element) => {
-            if (element != null) {
-                element.onclick = (ev) => {
-                    //get attribute value
-                    const retrievedElementType = element.getAttribute("data-element-type");
-                    if (retrievedElementType === null) {
-                        alert("missing components, please refresh the page!!!");
-                        return;
-                    }
-                    const formElement = new FormElements();
-                    ev.preventDefault();
-                    const divHeadingWrapper = formElement.FindFormElementToCreate(retrievedElementType);
-                    if (divHeadingWrapper != null) {
-                        this._customFormSection.append(divHeadingWrapper);
-                    }
-                };
-            }
+        const _formElements = document.querySelectorAll(".formElementWrapper");
+        _formElements.forEach((element) => {
+            this.AddFormClick(element);
         });
+    }
+    AddFormClick(element) {
+        if (element != null) {
+            element.onclick = (ev) => {
+                //get attribute value
+                const retrievedElementType = element.getAttribute("data-element-type");
+                if (retrievedElementType === null) {
+                    alert("missing components, please refresh the page!!!");
+                    return;
+                }
+                const formElement = new FormElements();
+                ev.preventDefault();
+                const divHeadingWrapper = formElement.FindFormElementToCreate(retrievedElementType);
+                if (divHeadingWrapper != null) {
+                    this._customFormSection.append(divHeadingWrapper);
+                }
+            };
+            //this.AddFormDrag(element);
+        }
+    }
+    AddFormDrag(element) {
+        //check if element is being dragged
+        element.ondragstart = (ev) => {
+            var _a;
+            console.log(element.getAttribute("data-element-type"));
+            (_a = ev.dataTransfer) === null || _a === void 0 ? void 0 : _a.setData("text", element.getAttribute("data-element-type"));
+        };
+        //check if element is being dragged over
+        this._customFormSection.ondragover = (ev) => {
+            ev.preventDefault();
+        };
+        //check if element is being dropped
+        this._customFormSection.ondrop = (ev) => {
+            var _a;
+            debugger;
+            ev.preventDefault();
+            const data = (_a = ev.dataTransfer) === null || _a === void 0 ? void 0 : _a.getData("text").trim();
+            const test = element.getAttribute("data-element-type");
+            if (data === null) {
+                alert("missing components, please refresh the page!!!");
+                return;
+            }
+            const formElement = new FormElements();
+            const divHeadingWrapper = formElement.FindFormElementToCreate(test);
+            if (divHeadingWrapper != null) {
+                element.after(divHeadingWrapper);
+                //this._customFormSection.append(divHeadingWrapper);
+            }
+            element.remove();
+            this.AddFormElement();
+        };
+    }
+    AddElementFromDrag(element) {
+        const formElement = new FormElements();
+        const divHeadingWrapper = formElement.FindFormElementToCreate(element.getAttribute("data-element-type"));
+        if (divHeadingWrapper != null) {
+            element.after(divHeadingWrapper);
+        }
+        element.remove();
+        this.AddFormElement();
     }
 }
 //# sourceMappingURL=FormBuilder.js.map
--- MySaaSProject/wwwroot/typescript/FormBuilder.ts
+++ MySaaSProject/wwwroot/typescript/FormBuilder.ts
@@ -3,32 +3,87 @@
 
 class FormBuilder {
     private _customFormSection = document.querySelector("#customFormSection") as HTMLDivElement;
-    private readonly _formElements = document.querySelectorAll(".formElementWrapper") as NodeListOf<HTMLDivElement>;
+    /*private readonly _formElements = document.querySelectorAll(".formElementWrapper") as NodeListOf<HTMLDivElement>;*/
 
     constructor() {
         this.AddFormElement();
     }
 
     private AddFormElement(): void {
-        this._formElements.forEach((element) => {
-            if (element != null) {
-                element.onclick = (ev: MouseEvent) => {
-                    //get attribute value
-                    const retrievedElementType = element.getAttribute("data-element-type") as string;
-                    if (retrievedElementType === null) {
-                        alert("missing components, please refresh the page!!!");
-                        return;
-                    }
-
-                    const formElement: FormElements = new FormElements();
-                    ev.preventDefault();
-                    const divHeadingWrapper = formElement.FindFormElementToCreate(retrievedElementType);
-
-                    if (divHeadingWrapper != null) {
-                        this._customFormSection.append(divHeadingWrapper);
-                    }
-                };
-            }
+        const _formElements = document.querySelectorAll(".formElementWrapper") as NodeListOf<HTMLDivElement>;
+        _formElements.forEach((element) => {
+            this.AddFormClick(element);
         });
+    }
+
+    private AddFormClick(element: any): void {
+        if (element != null) {
+            element.onclick = (ev: MouseEvent) => {
+                //get attribute value
+                const retrievedElementType = element.getAttribute("data-element-type") as string;
+                if (retrievedElementType === null) {
+                    alert("missing components, please refresh the page!!!");
+                    return;
+                }
+
+                const formElement: FormElements = new FormElements();
+                ev.preventDefault();
+                const divHeadingWrapper = formElement.FindFormElementToCreate(retrievedElementType);
+
+                if (divHeadingWrapper != null) {
+                    this._customFormSection.append(divHeadingWrapper);
+                }
+            };
+
+            //this.AddFormDrag(element);
+        }
+    }
+
+    private AddFormDrag(element: HTMLDivElement): void {
+        //check if element is being dragged
+        element.ondragstart = (ev: DragEvent) => {
+            console.log(element.getAttribute("data-element-type"));
+            ev.dataTransfer?.setData("text", element.getAttribute("data-element-type") as string);
+        }
+
+        //check if element is being dragged over
+        this._customFormSection.ondragover = (ev: DragEvent) => {
+            ev.preventDefault();
+        }
+
+        //check if element is being dropped
+        this._customFormSection.ondrop = (ev: DragEvent) => {
+            debugger
+            ev.preventDefault();
+            const data = ev.dataTransfer?.getData("text").trim();
+            const test = element.getAttribute("data-element-type") as string;
+            if (data === null) {
+                alert("missing components, please refresh the page!!!");
+                return;
+            }
+
+            const formElement: FormElements = new FormElements();
+            const divHeadingWrapper = formElement.FindFormElementToCreate(test);
+
+            if (divHeadingWrapper != null) {
+                element.after(divHeadingWrapper);
+                //this._customFormSection.append(divHeadingWrapper);
+            }
+
+            element.remove();
+            this.AddFormElement();
+        }
+    }
+
+    public AddElementFromDrag(element: any): void {
+        const formElement: FormElements = new FormElements();
+        const divHeadingWrapper = formElement.FindFormElementToCreate(element.getAttribute("data-element-type") as string);
+
+        if (divHeadingWrapper != null) {
+            element.after(divHeadingWrapper);
+        }
+
+        element.remove();
+        this.AddFormElement();        
     }
 }
